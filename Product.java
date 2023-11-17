4042a17ae7315462d9132bc11965e41a0265e115
Saliha <saliha0768@gmail.com>
2023-11-17 15:20:40 +0200

    product

--- Product.java
+++ Product.java
@@ -1,3 +1,16 @@
 public class Product {
 
+    protected double price;
+
+    Product(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
